e4991ad5bc75171e5bdff2de61a3c6eaafb0c22e
mshahzaib4 <shahzaibmalik0448@gmail.com>
2025-12-11 02:50:47 -0800

    Initial commit

---
+++ template.sh
@@ -0,0 +1,16 @@
+mkdir -p src
+mkdir -p research
+
+# Creat files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch README.md
+touch requirements.txt
+
+touch research/trials.ipynb
+
+echo "Project structure created."
